697a7e3070d4d03860ccd7e46f69ae367c634b9c
Ngo Quoc Dat <datlechin@gmail.com>
2026-03-15 19:33:02 +0700

    fix: restore CODE_SIGN_STYLE=Manual with allowProvisioningUpdates

--- scripts/build-release.sh
+++ scripts/build-release.sh
@@ -345,6 +345,7 @@ build_for_arch() {
         -arch "$arch" \
         ONLY_ACTIVE_ARCH=YES \
         CODE_SIGN_IDENTITY="$SIGN_IDENTITY" \
+        CODE_SIGN_STYLE=Manual \
         DEVELOPMENT_TEAM="$TEAM_ID" \
         ${ANALYTICS_HMAC_SECRET:+ANALYTICS_HMAC_SECRET="$ANALYTICS_HMAC_SECRET"} \
         -allowProvisioningUpdates \
